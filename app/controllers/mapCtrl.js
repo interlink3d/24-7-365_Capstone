c4435daa4f837467bdc92ff2afba9800b62586ee
interlink3d <interlink3d@gmail.com>
2016-09-16 14:55:40 -0500

    tweaked the mapDisplay partial to be split screen to display list results

--- app/controllers/mapCtrl.js
+++ app/controllers/mapCtrl.js
@@ -2,7 +2,7 @@
 
 app.controller("mapCtrl", function($scope, $location, $window, $sce, MapCalls){
 
-  $scope.map = { center: { latitude: 36.16, longitude: -86.78 }, zoom: 15 };
+  $scope.map = { center: { latitude: 36.16, longitude: -86.78 }, zoom: 20 };
 
   $scope.mySearchUrl = MapCalls.getUrl();
 
